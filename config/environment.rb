91eaf4f963d36d5735329df4331cd8119d1a1454
ljil;kj <schoeps.pat@gmail.com>
2022-12-18 20:40:47 -0800

    stash

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
 # Load the Rails application.
 require_relative "application"
 
-app_environment_variables = File.join(Rails.root, 'config', 'dev_env_vars.rb')
+app_environment_variables = File.join(Rails.root, 'config/sensitive', 'dev_env_vars.rb')
 load(app_environment_variables) if File.exists?(app_environment_variables)
 
 # Initialize the Rails application.
